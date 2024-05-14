0d2f3a1111b361e37a9154f04a619dfb43291302
hotteokButler <ygs12567@gmail.com>
2024-05-14 12:31:10 +0900

    Rename : not found 파일 이름 오탈자 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link'
- 
+import Link from 'next/link';
+
 export default function NotFound() {
   return (
     <div>
       <h2>Not Found</h2>
       <p>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</p>
-      <Link href="/search">검색</Link>
+      <Link href='/search'>검색</Link>
     </div>
-  )
+  );
 }
