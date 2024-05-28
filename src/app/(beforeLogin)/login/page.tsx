c058c0a158ecc871a2696f4760ace53aa6d70d6c
hotteokButler <ygs12567@gmail.com>
2024-05-28 10:54:42 +0900

    Fix : /login 접속 시 인터셉팅 라우팅 안되는 오류 수정

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,22 @@
-import { redirect } from 'next/navigation';
+'use client';
 
-export default function Login() {
+import { useRouter } from 'next/navigation';
+
+/*
+기본적으로 server component라서 
   redirect('/i/flow/login');
+하게되면 서버에서 redirect 시키기 때문에 intercepting routing이 동작하지 않는다
+(intercepting routing은 client에서 redirect 해야지 동작함)
+
+client routing 사용
+-> router.push의 경우 browser history stack에 등록
+      => 이럴경우 뒤로가기하면 다시 /login으로 오게되어 다시 /i/flow/login으로 리다이렉트 됨
+-> route.replace의 경우 browser history stack에 등록되지 x
+
+*/
+export default function Login() {
+  const router = useRouter();
+  router.replace('/i/flow/login', { scroll: false });
+  
+  return null;
 }
