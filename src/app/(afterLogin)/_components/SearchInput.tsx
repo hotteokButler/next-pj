0e47d137775e884ad583528748f5a61c8ab7c910
hotteokButler <ygs12567@gmail.com>
2024-06-14 23:50:11 +0900

    Feat : 검색 시 search 페이지로 넘기기
    - input default value 검색쿼리(q) 있을시 해당 value로 표시

--- src/app/(afterLogin)/_components/SearchInput.tsx
+++ src/app/(afterLogin)/_components/SearchInput.tsx
@@ -2,7 +2,9 @@
 
 import * as S from '@/components/style/searchCon.styled';
 import { GoSearch, GoArrowLeft } from 'react-icons/go';
-import { usePathname ,useRouter} from 'next/navigation';
+import { usePathname ,useRouter ,useSearchParams} from 'next/navigation';
+
+import { useRef } from 'react';
 interface IProps {
   isFixed: boolean;
 }
@@ -11,12 +13,21 @@ export default function SearchInput({ isFixed }: IProps) {
   const pathname = usePathname();
   const fPathname = pathname.replace('/', '');
   const router = useRouter();
+  const queryRef = useRef<HTMLInputElement>(null);
+  const searchParam = useSearchParams();
+  const nowSearchParam = searchParam.get('q');
 
-  const onClickBackBtn = (e: React.MouseEvent) => {
+
+  const onClickBackBtn : React.MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
     router.back();
   };
 
+  const onSubmit : React.FormEventHandler<HTMLFormElement> = e => {
+    e.preventDefault();
+    router.push( `/search?q=${queryRef.current?.value}`, { scroll: false })
+
+  };
   return (
     <S.SearchInputWrap $fixed={isFixed}>
       {['explore', 'search'].includes(String(fPathname)) && (
@@ -25,10 +36,10 @@ export default function SearchInput({ isFixed }: IProps) {
         </S.SearchBackBtn>
       )}
 
-      <S.SearchInputForm name='s_qurey' $fixed={isFixed}>
-        <S.SearchInputLabel htmlFor='search_query'>
+      <S.SearchInputForm name='s_qurey' $fixed={isFixed} onSubmit={onSubmit}>
+        <S.SearchInputLabel htmlFor='q'>
           <GoSearch />
-          <S.SearchInputEl id='search_query' name='search_query' placeholder='검색' />
+          <S.SearchInputEl id='q' name='q' ref={queryRef} placeholder='검색' defaultValue={ nowSearchParam ? nowSearchParam : '' } />
         </S.SearchInputLabel>
       </S.SearchInputForm>
     </S.SearchInputWrap>
