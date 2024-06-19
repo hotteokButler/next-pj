4363e478f6827aa40da2c62b81bb2beda5fc7610
hotteokButler <ygs12567@gmail.com>
2024-06-19 15:26:44 +0900

    Add : Search page dummy post 추가

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import ExploreMainCon from '../_components/ExploreMainCon';
+import Post from '../_components/Post';
 import SearchFixedTab from '../_components/SearchFixedTab';
 
 
@@ -18,6 +19,17 @@ export default function Search({searchParams} : IProps) {
   return (
     <ExploreMainCon>
       <SearchFixedTab q={searchParams.q} />
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
     </ExploreMainCon>
   );
 }
