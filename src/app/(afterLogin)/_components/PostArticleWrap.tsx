bf476483a1a1449263f9d338d6daf431a2b4ec90
hotteokButler <ygs12567@gmail.com>
2024-06-19 16:36:24 +0900

    Fix : Article click시에만 라우팅
    -프로필이나 이름 부분  링크 이동 안됨 수정

--- src/app/(afterLogin)/_components/PostArticleWrap.tsx
+++ src/app/(afterLogin)/_components/PostArticleWrap.tsx
@@ -2,17 +2,19 @@
 'use client';
 
 import { PostConentWrap } from '@/components/style/post.style';
-import React, { useRef } from 'react'
+import React from 'react'
 import { IData } from './Post';
-import { useRouter } from 'next/navigation';
+import { useRouter ,usePathname} from 'next/navigation';
 
 export default function PostArticleWrap({ children ,data }: { children: React.ReactNode , data : IData}) {
   const {postId ,User :{id}} = data;
   const router = useRouter();
+  const pathname = usePathname();
+  const pathnameArr = pathname.split('/')
 
   const onClick  = (e: React.MouseEvent<HTMLElement> ) => {
     e.preventDefault();
-    router.replace(`/${id}/status/${postId}`);
+   if( e.target.nodeName === 'ARTICLE' && !(pathnameArr.includes('status'))) router.replace(`/${id}/status/${postId}`);
   }
   return (
     <PostConentWrap onClick={onClick}>{children}</PostConentWrap>
