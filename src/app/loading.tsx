c82c53a60202ac17344fe81621892b90f6632553
hotteokButler <ygs12567@gmail.com>
2024-06-25 00:01:49 +0900

    Add : loading js - ui 보강해야함 아직 테스트

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+'use client';
+import * as S from '../components/style/common/commonStyle';
+
+
+export default function Loading () {
+  return <S.LoadingWrap>Loading.....</S.LoadingWrap>
+}
--- src/components/style/common/commonStyle.ts
+++ src/components/style/common/commonStyle.ts
@@ -87,3 +87,17 @@ export const ProfileFollowBtn = styled.button`
     background-color: ${(p) => p.theme.colors?.gray};
   }
 `;
+
+export const LoadingWrap = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.9);
+  z-index: 99;
+  color: #fff;
+`;
