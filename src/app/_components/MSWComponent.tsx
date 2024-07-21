cd05f91433facdaa74895d8730ca89f4e3c3f6b2
hotteokButler <ygs12567@gmail.com>
2024-07-21 22:01:00 +0900

    Comment : 주석 내용 수정

--- src/app/_components/MSWComponent.tsx
+++ src/app/_components/MSWComponent.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react'
 
 export default function MSWComponent() {
   useEffect(()=>{
-     if(typeof window !=='undefined') {
+     if(typeof window !=='undefined') { //typeof window === 'undefined' : 브라우저에서 돌아가는 걸 보장 해야 함
         if(process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
           require('@/mocks/browser');
         }
