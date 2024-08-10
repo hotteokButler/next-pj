b3eb254a5d37f95a881e8e20f88049df79d809da
hotteokButler <ygs12567@gmail.com>
2024-08-10 16:04:40 +0900

    ADD : React Query provider 설정
    - (afterLogin)/layout.tsx에만 react query provider 설정
    - provider은 client component이므로 따로 component 분리 후 설정

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -4,27 +4,28 @@ import LeftAsideCon from './_components/LeftAsideCon';
 import CenterMainCon from './_components/CenterMainCon';
 import RightAsideCon from './_components/RightAsideCon';
 import MainOuterCon from './_components/MainOuterCon';
+import RQProvider from './_components/RQProvider';
 
 interface IProps {
   children: ReactNode;
   modal: ReactNode;
 }
 
-export default function AfterLoginlayout({ children , modal}: IProps) {
-
-  
+export default function AfterLoginlayout({ children, modal }: IProps) {
   return (
-    <LayoutWrapper>
-      {/* left aside  ======== */}
-      <LeftAsideCon />
-      {/* main content  ======== */}
-      <MainOuterCon>
-        <CenterMainCon>{children}</CenterMainCon>
-        {/* right aside  ======== */}
-        <RightAsideCon />
-      </MainOuterCon>
-      {/* modal ======== */}
-      {modal}
-    </LayoutWrapper>
+    <RQProvider>
+      <LayoutWrapper>
+        {/* left aside  ======== */}
+        <LeftAsideCon />
+        {/* main content  ======== */}
+        <MainOuterCon>
+          <CenterMainCon>{children}</CenterMainCon>
+          {/* right aside  ======== */}
+          <RightAsideCon />
+        </MainOuterCon>
+        {/* modal ======== */}
+        {modal}
+      </LayoutWrapper>
+    </RQProvider>
   );
 }
