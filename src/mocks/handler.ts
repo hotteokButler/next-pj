85a8e0a59244a10bf6c0e85cda4c8c1903d5dc56
hotteokButler <ygs12567@gmail.com>
2024-08-10 16:14:02 +0900

    Add : Mock DAta

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,4 +1,20 @@
 import { http, HttpResponse } from 'msw';
+import { faker } from '@faker-js/faker';
+
+function generateDate() {
+  const lastWeek = new Date(Date.now());
+  lastWeek.setDate(lastWeek.getDate() - 7);
+  return faker.date.between({
+    from: lastWeek,
+    to: Date.now(),
+  });
+}
+const User = [
+  { id: 'user01', nickname: 'james', image: faker.image.avatar() },
+  { id: 'user02', nickname: 'ruru', image: faker.image.avatar() },
+  { id: 'user03', nickname: '레오', image: faker.image.avatar() },
+];
+const Posts = [];
 
 //  대신 응답 보낼 부분
 
@@ -7,7 +23,7 @@ export const handler = [
     console.log('로그인');
 
     return HttpResponse.json(
-      {id: 'anonymous02', nickname: 'anonymous', image: '/user_02.jpg', isPrivate: 'false', role: 'user'},
+      { id: 'anonymous02', nickname: 'anonymous', image: '/user_02.jpg', isPrivate: 'false', role: 'user' },
       {
         headers: {
           'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
@@ -32,8 +48,61 @@ export const handler = [
     // });
     return HttpResponse.text(JSON.stringify('ok'), {
       headers: {
-        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
-      }
-    })
+        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
+      },
+    });
+  }),
+  http.get('/api/postRecommends', ({ request }) => {
+    const url = new URL(request.url);
+    const cursor = parseInt(url.searchParams.get('cursor') as string) || 0;
+    return HttpResponse.json([
+      {
+        postId: cursor + 1,
+        User: User[0],
+        content: `${cursor + 1} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+        createdAt: generateDate(),
+      },
+      {
+        postId: cursor + 2,
+        User: User[0],
+        content: `${cursor + 2} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [
+          { imageId: 1, link: faker.image.urlLoremFlickr() },
+          { imageId: 2, link: faker.image.urlLoremFlickr() },
+        ],
+        createdAt: generateDate(),
+      },
+      {
+        postId: cursor + 3,
+        User: User[0],
+        content: `${cursor + 3} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [],
+        createdAt: generateDate(),
+      },
+      {
+        postId: cursor + 4,
+        User: User[0],
+        content: `${cursor + 4} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [
+          { imageId: 1, link: faker.image.urlLoremFlickr() },
+          { imageId: 2, link: faker.image.urlLoremFlickr() },
+          { imageId: 3, link: faker.image.urlLoremFlickr() },
+          { imageId: 4, link: faker.image.urlLoremFlickr() },
+        ],
+        createdAt: generateDate(),
+      },
+      {
+        postId: cursor + 5,
+        User: User[0],
+        content: `${cursor + 5} Z.com is so marvelous. I'm gonna buy that.`,
+        Images: [
+          { imageId: 1, link: faker.image.urlLoremFlickr() },
+          { imageId: 2, link: faker.image.urlLoremFlickr() },
+          { imageId: 3, link: faker.image.urlLoremFlickr() },
+        ],
+        createdAt: generateDate(),
+      },
+    ]);
   }),
 ];
